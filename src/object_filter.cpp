0715abb5f98a932ba0746baf9d583bd3e16c37ea
Huong Do Van <33709616+dovanhuong@users.noreply.github.com>
2018-12-11 16:07:11 +0900

    Update object_filter.cpp

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -1,5 +1,5 @@
 /* Copyright by Huong Do Van - 10/11/2018
-Any suggestion or advice, pls send via email: dvhbkhn@gmail.com
+Any suggestion or advice, pls send via email: vanhuong.robotics@gmail.com
 */
 #include <ros/ros.h>
 #include <opencv2/imgproc/imgproc.hpp>
